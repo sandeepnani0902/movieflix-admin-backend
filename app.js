8f2023fe3fa8284e9f3ee52c85743a8c577090bb
sandeepnani0902 <sandeepnani0902@gmail.com>
2026-05-24 12:16:53 +0530

    CHANGES IN APP.JS

--- app.js
+++ app.js
@@ -40,15 +40,15 @@ app.use('/movieflix', genreRouter);
 app.use("/movieflix", movieRouter);
 app.use("/movieflix", WebSeriesRouter)
 app.use("/movieflix", SeasonsRouter)
-app.use("/movieflix",EpisodeRouter)
+app.use("/movieflix", EpisodeRouter)
 // app.use("/movieflix", addmovieRouter)
 // catch 404
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
